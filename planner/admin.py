5ae20f5fc09a87258f94425cc732731ff8f31c37
Paul Schlais <pschlais@gmail.com>
2018-09-14 12:32:14 -0700

    Registered DriveTimeMajorCity to admin interface

--- planner/admin.py
+++ planner/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Destination, Route, Trailhead, County, Jurisdiction, GoverningBody, MajorCity, Profile
+from .models import Destination, Route, Trailhead, County, Jurisdiction, GoverningBody, MajorCity, Profile, DriveTimeMajorCity
 
 # Register your models here.
 admin.site.register(Destination)
@@ -10,3 +10,11 @@ admin.site.register(Jurisdiction)
 admin.site.register(GoverningBody)
 admin.site.register(MajorCity)
 admin.site.register(Profile)
+
+# Admin classes
+@admin.register(DriveTimeMajorCity)
+class DriveTimeMajorCityAdmin(admin.ModelAdmin):
+    list_display = ('majorcity', 'trailhead', 'drive_time',
+        'drive_distance', 'date_updated', 'api_call_status', 'error_message')
+    list_filter = ('majorcity', 'trailhead', 'drive_time',
+        'drive_distance', 'date_updated', 'api_call_status', 'error_message')
